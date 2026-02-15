319bb11440b3c90c6e2236550a890a0a707c26a9
TriNauD <tri.san@outlook.com>
2026-02-15 21:40:46 +0800

    v2-RELEASE: 从POI迁移至easyExcel

--- src/main/java/com/bank/rcm/service/ExcelService.java
+++ src/main/java/com/bank/rcm/service/ExcelService.java
@@ -3,6 +3,7 @@ package com.bank.rcm.service;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -12,11 +13,35 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.context.AnalysisContext;
+import com.alibaba.excel.event.AnalysisEventListener;
 import com.bank.rcm.dto.StepBDto;
 import com.bank.rcm.entity.ComplianceInventory;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class ExcelService {
+
+    // 泛式，增加可扩展性，以后换成别的DTO也能使
+    // 原数据流/dto/具体处理方法
+    public <T> void readExcelInStream(InputStream inputStream, Class<T> clazz, Consumer<T> rowProcessor) {
+        EasyExcel.read(inputStream, clazz, new AnalysisEventListener<T>() {
+            @Override
+            public void invoke(T data, AnalysisContext context) {
+                // 调用传入的处理逻辑
+                rowProcessor.accept(data);
+            }
+
+            @Override
+            public void doAfterAllAnalysed(AnalysisContext context) {
+                log.info("Excel 读取完成");
+            }
+        }).sheet().doRead();
+    }
+
     public List<ComplianceInventory> mockParseExcel() {
         List<ComplianceInventory> list = new ArrayList<>();
 
@@ -64,7 +89,7 @@ public class ExcelService {
             Sheet sheet = workbook.getSheetAt(0);
 
             for (Row row : sheet) {
-                if (row.getRowNum() ==0) {
+                if (row.getRowNum() == 0) {
                     continue;
                 }
                 StepBDto stepBDto = new StepBDto();
@@ -74,7 +99,7 @@ public class ExcelService {
                 stepBDto.setCeamIds(getCellValue(row.getCell(3)));
                 list.add(stepBDto);
             }
-        } 
+        }
 
         return list;
     }
